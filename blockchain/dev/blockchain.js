7e97eb3de95cbae7334213d25eaf513466a2a1c3
zentaker <46875264+zentaker@users.noreply.github.com>
2019-07-06 09:07:33 -0500

    Add files via upload

---
+++ blockchain/dev/blockchain.js
@@ -0,0 +1,64 @@
+function Blockchain () {
+	this.chain = []; //donde la cadena se va a almacenar los bloques que se minen
+	this.newTransactions =[]; // donde se van a retener todas las transaciones antes de bloque
+}
+
+// creacion de un metodo para la creacion de bloques mediante un prototype object 
+
+Blockchain.prototype.createNewBlock = function( nonce, previousBlockHash, hash ){ // se le dan 3 parametros a la funcion 
+	const newBlock = { // aca es donde toda las propiedades se va a almacenar 
+		
+		index: this.chain.length + 1, // el numero del bloque 
+		timestamp: Date.now(), // para saber cuando el bloque fue creado 
+		transaction: this.pendingTransaction, // para crear el bloque y este sea introducido y no se pueda cambiar 
+		nonce: nonce, // viene del prof of work
+		hash: hash, 
+		previousBlockHash: previousBlockHash,
+	}; 
+
+	
+	this.pendingTransaction = []; // para poder crear un nuevo bloque de cero 
+	this.chain.push(newBlock); // para empujarla en la cadena 
+	return newBlock;
+	
+};	
+
+// creando el metodo del ultimo bloque 
+
+Blockchain.prototype.getLastBlock = function(){ 
+
+	return this.chain[this.chain.length - 1]; // define la posicion del bloque anterios al negarlo en 1
+};
+
+// creando el metodo para la nueva transicion 
+
+Blockchain.prototype.createNewTransaction = function( amount, sender, recipient ) {
+	const newTransaction = {
+		amount: amount, //este parametro de cuanto se esta enviando en la transacion 
+		sender: sender, // este va a tomar la direccion del que envia 
+		recipient: recipient, //tomar la direccion del que recive
+	}
+
+return newTransaction;
+
+}
+
+Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj) {
+	this.pendingTransactions.push(transactionObj);
+	return this.getLastBlock()['index'] + 1;
+};
+
+// hashBlock method
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) { // es la data que se va a juntar para crear el hash
+
+	const sha256 = require('sha256'); // importar la libreria SHA256
+	const dataAsString = previousBlockHash + nonce.toString(int) + JSON.stringify(currentBlockData);
+	const hash = sha256(dataAsString); // asi es como se crea el hash del bloque y todos los demas que pasan por la funcion
+	return hash;
+
+}
+
+
+module.exports = Blockchain; // para exportar la constructor function
+
---
+++ blockchain/dev/test.js
@@ -0,0 +1,43 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.hashBlock();
+
+/*const previousBlockHash = '687765DA6CCF0668238C1D372737DS11';
+
+const currentBlockData = [{
+	amount: 10,
+	sender: 'B4CEE9C0E571',
+	recipient: '3A3FG6E462D48E9',
+}];
+
+
+
+const nonce = 100;
+
+bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));*/
+
+
+
+bitcoin.createNewBlock(2389, 'OIUOEREEDHKDE', '78s97d4x6dsf');
+
+bitcoin.createNewTransaction(100, 'ALEXH845SJ5TKCJ2', 'JENN5BG5DF6HTGFAD');
+
+bitcoin.createNewBlock(2389, 'OIUOEREEDHKDE', '78s97d4x6dsf');
+
+bitcoin.createNewTransaction(100, 'ALEXH845SJ5TKCJ2', 'JENN5BG5DF6HTGFAD');
+bitcoin.createNewTransaction(60, 'ALEXH845SJ5TKCJ2', 'JENN5BG5DF6HTGFAD');
+bitcoin.createNewTransaction(400, 'ALEXH845SJ5TKCJ2', 'JENN5BG5DF6HTGFAD');
+
+bitcoin.createNewBlock(2389, 'OIUOEREEDHKDE', '78s97d4x6dsf');
+
+
+
+
+
+
+
+
+console.log(bitcoin); 
